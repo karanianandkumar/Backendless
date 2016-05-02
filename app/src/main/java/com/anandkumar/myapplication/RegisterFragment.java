1c15a5b0de4e194f33877487c4e7cd735d2d13c2
karanianandkumar <karanianandkumar@yahoo.com>
2016-05-02 19:17:31 +0530

    4.6 : Register to backendless

--- app/src/main/java/com/anandkumar/myapplication/RegisterFragment.java
+++ app/src/main/java/com/anandkumar/myapplication/RegisterFragment.java
@@ -6,6 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
 
 
 /**
@@ -13,6 +21,10 @@ import android.view.ViewGroup;
  */
 public class RegisterFragment extends Fragment {
 
+
+    Button registerButton;
+    EditText usernameField;
+    EditText passwordField;
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -23,7 +35,41 @@ public class RegisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View view= inflater.inflate(R.layout.fragment_register, container, false);
+
+        usernameField=(EditText)view.findViewById(R.id.userNameField);
+        passwordField=(EditText)view.findViewById(R.id.passwordField);
+
+
+        registerButton=(Button)view.findViewById(R.id.register);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username=usernameField.getText().toString();
+                String password=passwordField.getText().toString();
+
+
+                BackendlessUser backendlessUser=new BackendlessUser();
+                backendlessUser.setPassword(password);
+                backendlessUser.setProperty("name",username);
+
+                Backendless.UserService.register(backendlessUser, new AsyncCallback<BackendlessUser>() {
+                    @Override
+                    public void handleResponse(BackendlessUser response) {
+                        Toast.makeText(getActivity(),"Successfully Register",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void handleFault(BackendlessFault fault) {
+                        Toast.makeText(getActivity(),"User not Registered",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+
+        return view;
     }
 
 }
